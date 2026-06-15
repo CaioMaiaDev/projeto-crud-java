f0c855be401a9d7af12da02bd31f1de92df7b387
Caio Henrique <caiohenriqueleal477@gmail.com>
2026-06-15 11:00:55 -0300

    recriando o repositorio de produtos

--- src/main/java/com/example/crud/domain/product/ProductRepository.java
+++ src/main/java/com/example/crud/domain/product/ProductRepository.java
@@ -3,4 +3,5 @@ package com.example.crud.domain.product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProductRepository extends JpaRepository<Product, String> {
+
 }
